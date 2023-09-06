09683d06227927f0bf9e106a1ec1063a06f6af37
Andre Gabriel Vasques <andre.vasques@jacarei.local>
2023-09-06 10:14:00 -0300

    att

--- frontend/src/StateProvider.js
+++ frontend/src/StateProvider.js
@@ -5,7 +5,7 @@ import React, { createContext, useContext, useReducer } from "react";
 export const StateContext = createContext();
 
 // estado inicial e função redutora
-const inicialState = {
+const initialState = {
     basket: [],
     //outros estados, se necessário
 };
